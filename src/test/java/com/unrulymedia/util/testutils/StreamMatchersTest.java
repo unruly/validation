f04dc1a7febd22f0699c6180c45c15738cf203e3
Andrew <unruly-oss@unrulymedia.com>
2015-05-06 10:23:30 +0000

    Renaming tests

--- src/test/java/com/unrulymedia/util/testutils/StreamMatchersTest.java
+++ src/test/java/com/unrulymedia/util/testutils/StreamMatchersTest.java
@@ -21,67 +21,67 @@ import static org.junit.Assert.fail;
 
 public class StreamMatchersTest {
     @Test
-    public void shouldNotMatchStreamsOfDifferingSingleItem() throws Exception {
+    public void equalTo_failureDifferingSingleItem() throws Exception {
         assertThat(Stream.of("a"), is(not(equalTo(Stream.of("b")))));
     }
 
     @Test
-    public void shouldNotMatchStreamOfDifferingSingleItem() throws Exception {
+    public void contains_failureDifferingSingleItem() throws Exception {
         assertThat(Stream.of("a"), is(not(contains("b"))));
     }
 
     @Test
-    public void shouldNotMatchStreamsOfDifferingLength() throws Exception {
+    public void equalTo_failureDifferingLength() throws Exception {
         assertThat(Stream.of("a"), is(not(equalTo(Stream.of("a", "b")))));
     }
 
     @Test
-    public void shouldNotMatchStreamOfDifferingLength() throws Exception {
+    public void contains_failureDifferingLength() throws Exception {
         assertThat(Stream.of("a"), is(not(contains("a", "b"))));
     }
 
     @Test
-    public void shouldNotMatchStreamsOfDifferingItems() throws Exception {
+    public void equalTo_failureDifferingItems() throws Exception {
         assertThat(Stream.of("a","c"), is(not(equalTo(Stream.of("a", "b")))));
     }
 
     @Test
-    public void shouldNotMatchStreamOfDifferingItems() throws Exception {
+    public void contains_failureDifferingItems() throws Exception {
         assertThat(Stream.of("a","c"), is(not(contains("a", "b"))));
     }
 
     @Test
-    public void shouldMatchEmptyStreams() throws Exception {
+    public void equalTo_successEmpty() throws Exception {
         assertThat(Stream.empty(), equalTo(Stream.empty()));
     }
 
     @Test
-    public void emptyStream() throws Exception {
+    public void empty_Success() throws Exception {
         assertThat(Stream.empty(),empty());
     }
 
     @Test
-    public void nonEmptyStreamIsNotEmpty() throws Exception {
+    public void empty_Failure() throws Exception {
         testFailingMatcher(empty(),Stream.of(3),"An empty Stream","A non empty Stream starting with <3>");
     }
 
     @Test
-    public void shouldMatchIntStream() throws Exception {
+    public void equalToIntStream_success() throws Exception {
         assertThat(IntStream.range(1, 10), equalTo(IntStream.range(1, 10)));
     }
 
     @Test
-    public void shouldMatchIntStream_varargs() throws Exception {
+    public void containsIntStream_success() throws Exception {
         assertThat(IntStream.range(1, 4), contains(1,2,3));
     }
 
     @Test
-    public void shouldMatchIdenticalStreams() throws Exception {
+    public void equalTo_successManyItems() throws Exception {
         assertThat(Stream.of("a", "b", "c"), equalTo(Stream.of("a", "b", "c")));
     }
 
     @Test
-    public void shouldMatchStreamWithIdenticalItems() throws Exception {
+    public void contains_successManyItems() throws Exception {
         assertThat(Stream.of("a", "b", "c"), contains("a", "b", "c"));
     }
 
@@ -120,22 +120,22 @@ public class StreamMatchersTest {
     }
 
     @Test
-    public void shouldMatchUpToLimit() throws Exception {
+    public void startsWithMatcher_success() throws Exception {
         assertThat(Stream.iterate(0,i -> i + 1), startsWith(Stream.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9), 10));
     }
 
     @Test
-    public void shouldMatchStartingItems() throws Exception {
+    public void startsWithItems_success() throws Exception {
         assertThat(Stream.of("a","b","c","d","e", "f", "g", "h"), startsWith("a", "b", "c", "d", "e"));
     }
 
     @Test
-    public void shouldMatchStartingItems_int() throws Exception {
+    public void startsWithItemsIntStream_success() throws Exception {
         assertThat(IntStream.range(0,Integer.MAX_VALUE), startsWithInt(0, 1, 2, 3, 4));
     }
 
     @Test
-    public void shouldShowExpectedAndActualIfError_equalTo() throws Exception {
+    public void equalTo_failureMessages() throws Exception {
         Matcher<BaseStream<String, Stream<String>>> matcher = equalTo(Stream.of("a", "b", "c", "d", "e", "f", "g", "h"));
         Stream<String> testData = Stream.of("a", "b", "c", "d", "e");
         testFailingMatcher(matcher, testData,"Stream of [\"a\",\"b\",\"c\",\"d\",\"e\",\"f\",\"g\",\"h\"]","Stream of [\"a\",\"b\",\"c\",\"d\",\"e\"]");
@@ -143,14 +143,14 @@ public class StreamMatchersTest {
 
 
     @Test
-    public void shouldShowExpectedAndActualIfError_contains() throws Exception {
+    public void contains_failureMessages() throws Exception {
         Stream<String> testData = Stream.of("a", "b", "c", "d", "e");
         Matcher<BaseStream<String, Stream<String>>> matcher = contains("a", "b", "c", "d", "e", "f", "g", "h");
         testFailingMatcher(matcher,testData,"Stream of [\"a\",\"b\",\"c\",\"d\",\"e\",\"f\",\"g\",\"h\"]","Stream of [\"a\",\"b\",\"c\",\"d\",\"e\"]");
     }
 
     @Test
-    public void shouldShowErrorOfBaseStream() throws Exception {
+    public void equalToIntStream_failureMessages() throws Exception {
         IntStream testData = IntStream.range(8, 10);
         Matcher<BaseStream<Integer, IntStream>> matcher = equalTo(IntStream.range(0, 6));
         testFailingMatcher(matcher,testData,"Stream of [<0>,<1>,<2>,<3>,<4>,<5>]", "Stream of [<8>,<9>]");
